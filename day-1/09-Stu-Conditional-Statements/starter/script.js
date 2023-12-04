95345d8955c029f6ecbfac84714cd4e709667fbd
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-04 22:45:17 +0000

    Day 1 task 4

--- day-1/09-Stu-Conditional-Statements/starter/script.js
+++ day-1/09-Stu-Conditional-Statements/starter/script.js
@@ -1,6 +1,24 @@
 // Change the values and operators below to test your algorithm meets all conditions
 var x = 50;
-var expression1 = (x < 25);
-var expression2 = (x > 50);
+var expression1 = x < 25;
+var expression2 = x > 50;
 
 // Write Your JavaScript Code Here
+
+// Check if both expressions are true using &&
+
+if (expression1 && expression2) {
+  console.log("true true");
+
+  // If both conditions are not true, check if expression1 is true
+} else if (expression1) {
+  console.log("true false");
+
+  // If expression1 is not true, then check if expression2 is true
+} else if (expression2) {
+  console.log("false true");
+
+  // If none of the conditions above evaluate to true, both expressions must be false
+} else {
+  console.log("false false");
+}
