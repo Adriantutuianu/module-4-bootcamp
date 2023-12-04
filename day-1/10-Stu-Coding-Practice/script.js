1cf3759286971c4c86116f22155a6f52d5c5a44c
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-04 23:11:35 +0000

    Luxury tax calculator if statement

---
+++ day-1/10-Stu-Coding-Practice/script.js
@@ -0,0 +1,21 @@
+console.log("ciao");
+
+var player1 = 20000000;
+var player2 = 19000000;
+var player3 = 15000000;
+
+var sum = player1 + player2 + player3;
+console.log(sum + " $");
+
+var limit = 40000000;
+var taxrate = 0.18;
+var excees = sum - limit;
+var luxuryTax = excees * taxrate;
+
+console.log(excees);
+
+if (sum > limit) {
+  console.log(luxuryTax + " Luxury tax");
+} else {
+  console.log("No luxury tax applied");
+}
