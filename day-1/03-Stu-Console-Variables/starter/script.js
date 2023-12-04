f33d0abdcf4c54943764116c0fea40f9f58ad4d4
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-04 21:22:28 +0000

    Day 1 first task

--- day-1/03-Stu-Console-Variables/starter/script.js
+++ day-1/03-Stu-Console-Variables/starter/script.js
@@ -1,1 +1,8 @@
 // Write Your JavaScript Code Here
+var personName = "Adrian Tutuianu";
+var pets = 2;
+var funFact = "I love water:)";
+
+console.log('My name is ' + personName +'.');
+console.log('I have ' + pets + 'pets.');
+console.log('Fun fact: ' + funFact + '.');
