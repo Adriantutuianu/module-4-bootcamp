f60111b3c19a5bd8c718c75153c18e668d8c21f4
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-06 00:29:21 +0000

    Arrays- for loop

--- day-2/04-Stu-Iteration/starter/script.js
+++ day-2/04-Stu-Iteration/starter/script.js
@@ -1,1 +1,10 @@
 //WRITE YOUR CODE BELOW
+
+var students = ["John", "Andrew", "Alex", "Max", "George"];
+console.log(students.length + " total number of elements");
+
+// I used for loop to display each name in console log
+for (let i = 0; i < students.length; i++) {
+  // Log the welcome message for each student
+  console.log("Great to see you, " + students[i] + "!");
+}
