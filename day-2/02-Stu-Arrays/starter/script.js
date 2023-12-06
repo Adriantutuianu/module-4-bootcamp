d7866aa38d139c0348e72971b03f7f60c19cddb9
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-06 00:12:11 +0000

    Arrays ex

--- day-2/02-Stu-Arrays/starter/script.js
+++ day-2/02-Stu-Arrays/starter/script.js
@@ -1,1 +1,23 @@
 // WRITE YOUR CODE HERE
+
+var studentName = ["student1", "student2", "student3", "student4", "student5"];
+console.log("Total numbers of elements in the array is: " + studentName.length);
+
+// I used for loop to display each name in console log
+for (let i = 0; i < studentName.length; i++) {
+  // Log the welcome message for each student
+  console.log("Welcome to the class " + studentName[i]);
+}
+
+// Store the original name at a specific index
+var indexToReplace = 0;
+var originalName = studentName[indexToReplace];
+
+// replaced one student with another
+studentName[0] = "Student43";
+console.log("student1 is replace with " + studentName[0]);
+
+// Check if the element has been replaced
+if (originalName !== studentName[indexToReplace]) {
+  console.log(studentName[indexToReplace] + " is in the class");
+}
