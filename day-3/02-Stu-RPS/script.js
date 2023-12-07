3b5bba3d39e8b6972ece3db5c44aed629b91dda5
Adrian Tut <adriantutuianu01@gmail.com>
2023-12-07 19:44:46 +0000

    Prompt user to enter "r," "p," or "s".

--- day-3/02-Stu-RPS/script.js
+++ day-3/02-Stu-RPS/script.js
@@ -1,2 +1,6 @@
 // 1. Initialize `choices` array: r, p, s. representing rock, paper, or scissors.
-choices = ["r", "p", "s"];
+var choices = ["r", "p", "s"];
+console.log(choices);
+
+// 2. Prompt user to enter "r," "p," or "s".
+var whatChoice = prompt("Please choose between r, p, or s");
